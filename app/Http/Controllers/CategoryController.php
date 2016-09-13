2e512511552bee358f32dfefbfed58148d71d9cc
fabio92rz <fabio92rz@gmail.com>
2016-09-13 10:16:33 +0200

    CategoryController added

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Comments;
+use App\Model\Posts;
+use App\Model\Categories;
+use App\User;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostFormRequest;
+
+
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+
+        $categories = Categories::orderBy('id', 'desc')->get();
+        return view('category.create')->withCategories($categories);
+
+
+    }
+
+    public function create()
+    {
+
+        $categories = Categories::orderBy('id', 'desc')->get();
+        return view('category.create')->withCategories($categories);
+    }
+
+    public function store(Request $request)
+    {
+
+        $categories = Categories::orderBy('id', 'desc')->get();
+        $category = new Categories();
+        $category->timestamps = false;
+        $category->category = $request->get('category');
+
+        if ($request->has('save')) {
+            $message = 'Categoria aggiunta!';
+        } else {
+            $message = 'Categoria Aggiunta!';
+        }
+
+        $category->save();
+
+        return redirect('new-category/')->withMessage($message)->withCategories($categories);
+    }
+
+    public function delete($category_id)
+    {
+
+        $id = Input::get('category_id');
+        var_dump($id);
+
+        $category = Categories::find($id);
+        $categories = Categories::orderBy('id', 'desc')->get();
+
+        $category->delete();
+        $message = "Categoria eliminata";
+
+        return redirect('new-category/')->withMessage($message)->withCategories($categories);
+
+    }
+
+    public function edit(Request $request)
+    {
+        $categories = Categories::orderBy('id', 'desc')->get();
+        $category_id = $request->input('id');
+        $category = Posts::find($category_id);
+
+            $category->category = $request->get('category');
+
+            if ($request->has('save')) {
+                $message = 'Post saved successfully';
+            } else {
+                $message = 'Post updated successfully';
+            }
+            $category->save();
+
+        return redirect('new-category/')->withMessage($message)->withCategories($categories);
+
+    }
+
+}
